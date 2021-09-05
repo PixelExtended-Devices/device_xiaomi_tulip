f2387fa104096a561d4be7ceac4d6e02510851a1
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-05 20:04:02 +0530

    tulip: Added Refin August Fp

--- aosp_tulip.mk
+++ aosp_tulip.mk
@@ -47,3 +47,8 @@ PEX_MAINTAINER := PinCredible
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := tulip
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
